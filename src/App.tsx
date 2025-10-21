b85b2c292fb782208fdb19cf844420bcfd3b5178
Mark Levente Varga <vargamarklevente@gmail.com>
2025-10-22 00:31:51 +0200

    continue setup
    - add .env
    - update gitignore
    - create LoginPage
    - set up intial folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,6 @@
-import "./App.css";
-import { Field, Input, Label } from "@headlessui/react";
-
+import LoginPage from "./pages/LoginPage";
 function App() {
-  return (
-    <div className="h-svh flex flex-col gap-8 justify-center items-center">
-      <Field className="flex flex-col">
-        <Label>Username</Label>
-        <Input
-          className="border border-amber-600"
-          name="username"
-          type="text"
-        />
-      </Field>
-      <Field className="flex flex-col">
-        <Label>Password</Label>
-        <Input
-          className="border border-amber-600"
-          name="password"
-          type="password"
-        />
-      </Field>
-    </div>
-  );
+  return <LoginPage />;
 }
 
 export default App;
